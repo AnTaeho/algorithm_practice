544dde693fb6c493783644d5d8fac6b587a54136
antaeho <an981022@naver.com>
2023-03-20 13:16:24 +0900

    1번 2번 해결

---
+++ src/main/java/org/bronze/No1000.java
@@ -0,0 +1,15 @@
+package org.bronze;
+
+import java.util.Scanner;
+
+public class No1000 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a+b);
+    }
+}
---
+++ src/main/java/org/bronze/No2557.java
@@ -0,0 +1,7 @@
+package org.bronze;
+
+public class No2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
