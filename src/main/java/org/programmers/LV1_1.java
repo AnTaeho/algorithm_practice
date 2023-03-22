fb95e514510050bc9a583627222a09e4afcfb79c
antaeho <an981022@naver.com>
2023-03-22 09:58:01 +0900

    6번 해결

---
+++ src/main/java/org/programmers/LV1_1.java
@@ -0,0 +1,21 @@
+package org.programmers;
+
+public class LV1_1 {
+    public static void main(String[] args) {
+        String t = "10203";
+        String p = "15";
+        System.out.println(solution(t, p));
+    }
+
+    public static int solution(String t, String p) {
+        int len = p.length();
+        long num = Long.parseLong(p);
+        int result = 0;
+
+        for (int i = 0; i < t.length() - len + 1; i++) {
+            long diff = Long.parseLong(t.substring(i, i + len));
+            if (diff <= num) result++;
+        }
+        return result;
+    }
+}
