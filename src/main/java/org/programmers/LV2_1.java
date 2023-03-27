d0b27a4a747bbce52748ecd4c258a8216532f1c0
antaeho <an981022@naver.com>
2023-03-27 11:53:31 +0900

    9번 해결

---
+++ src/main/java/org/programmers/LV2_1.java
@@ -0,0 +1,60 @@
+package org.programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class LV2_1 {
+    static HashMap<String, List<Integer>> map;
+
+    public static int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
+        map = new HashMap<>();
+
+        for (String s : info) {
+            String[] p = s.split(" ");
+            makeSentence(p, "", 0);
+        }
+        for (String key : map.keySet())
+            Collections.sort(map.get(key));
+
+        for (int i = 0; i < query.length; i++) {
+            query[i] = query[i].replaceAll(" and ", "");
+            String[] q = query[i].split(" ");
+            answer[i] = map.containsKey(q[0]) ? binarySearch(q[0], Integer.parseInt(q[1])) : 0;
+        }
+
+        return answer;
+    }
+
+    private static int binarySearch(String key, int score) {
+        List<Integer> list = map.get(key);
+        int start = 0, end = list.size() - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (list.get(mid) < score)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+
+        return list.size() - start;
+    }
+
+    private static void makeSentence(String[] p, String str, int cnt) {
+        if (cnt == 4) {
+            if (!map.containsKey(str)) {
+                List<Integer> list = new ArrayList<>();
+                map.put(str, list);
+            }
+            map.get(str).add(Integer.parseInt(p[4]));
+            return;
+        }
+        makeSentence(p, str + "-", 1);
+        makeSentence(p, str + p[cnt], cnt + 1);
+
+    }
+
+}
