a38a6ed0ad7d88bccff5ef95f66441621521dd4f
antaeho <an981022@naver.com>
2023-03-29 22:26:50 +0900

    11번 해결

---
+++ src/main/java/org/programmers/LV1_2.java
@@ -0,0 +1,22 @@
+package org.programmers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LV1_2 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(solution(nums));
+    }
+
+    private static int solution(int[] nums) {
+        int size = nums.length/2;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int set_size = set.size();
+
+        return Math.min(set_size, size);
+    }
+}
