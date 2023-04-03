15a670c0563852673d1d8aabc54d668f0739c519
antaeho <an981022@naver.com>
2023-04-03 13:37:19 +0900

    16번 해결

--- src/main/java/org/programmers/LV2_5.java
+++ src/main/java/org/programmers/LV2_5.java
@@ -1,14 +1,26 @@
 package org.programmers;
 
+import java.util.Stack;
+
 public class LV2_5 {
     public static void main(String[] args) {
 
     }
 
-    class Solution {
-        public int[] solution(int[] progresses, int[] speeds) {
-            int[] answer = {};
-            return answer;
+    private boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            }
+            else if(s.charAt(i) == ')') {
+                if (stack.empty()) {
+                    return false;
+                }
+                stack.pop();
+            }
         }
+        return stack.isEmpty();
     }
 }
