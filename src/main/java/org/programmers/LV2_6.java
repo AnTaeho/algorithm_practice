13de701ac006d0a41199159b6f9c72b153a0017c
antaeho <an981022@naver.com>
2023-04-03 14:13:28 +0900

    17번 해결

---
+++ src/main/java/org/programmers/LV2_6.java
@@ -0,0 +1,26 @@
+package org.programmers;
+
+public class LV2_6 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int total = brown+yellow;
+        for (int i = 1; i < total; i++) {
+            int row = i;
+            int col = total / i;
+            if (row > col) {
+                continue;
+            }
+            if ((row-2) * (col - 2) == yellow) {
+                answer[0] = col;
+                answer[1] = row;
+                return answer;
+            }
+        }
+        return answer;
+    }
+
+}
