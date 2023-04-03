8c0935e61402697346c0b9a3f0cc66991dba4fee
antaeho <an981022@naver.com>
2023-04-03 14:23:47 +0900

    18번 해결

---
+++ src/main/java/org/programmers/LV1_3.java
@@ -0,0 +1,25 @@
+package org.programmers;
+
+import java.util.Arrays;
+
+public class LV1_3 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[][] sizes) {
+        int hMax = 0;
+        int wMax = 0;
+        for (int[] size : sizes) {
+            int[] ints = Arrays.stream(size).sorted().toArray();
+            if (ints[0] > hMax) {
+                hMax = ints[0];
+            }
+            if (ints[1] > wMax) {
+                wMax = ints[1];
+            }
+        }
+        return hMax*wMax;
+    }
+}
