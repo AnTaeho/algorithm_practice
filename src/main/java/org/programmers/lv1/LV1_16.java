436c5b8043f83885de484a03e84456ae1271c5b0
antaeho <an981022@naver.com>
2023-04-17 10:09:17 +0900

    33번 해결

---
+++ src/main/java/org/programmers/lv1/LV1_16.java
@@ -0,0 +1,14 @@
+package org.programmers.lv1;
+
+public class LV1_16 {
+    public static void main(String[] args) {
+
+    }
+
+    public String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        }
+        return "Odd";
+    }
+}
