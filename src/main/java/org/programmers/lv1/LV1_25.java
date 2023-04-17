12539f4f78e76d6172ceeef6e4b39095f51c3d35
antaeho <an981022@naver.com>
2023-04-17 10:49:51 +0900

    42번 해결

---
+++ src/main/java/org/programmers/lv1/LV1_25.java
@@ -0,0 +1,12 @@
+package org.programmers.lv1;
+
+public class LV1_25 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+}
