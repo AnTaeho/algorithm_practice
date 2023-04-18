b03ae036c50bc55af6a0ee04d0e47622a9a3e998
antaeho <an981022@naver.com>
2023-04-18 09:15:03 +0900

    48번 해결

---
+++ src/main/java/org/programmers/lv1/LV1_31.java
@@ -0,0 +1,27 @@
+package org.programmers.lv1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LV1_31 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> solution(int[] arr, int divisor) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i : arr) {
+            if (i % divisor == 0) {
+                answer.add(i);
+            }
+        }
+        if (answer.isEmpty()) {
+            answer.add(-1);
+            return answer;
+        }
+        answer.sort(Comparator.naturalOrder());
+        return answer;
+    }
+}
