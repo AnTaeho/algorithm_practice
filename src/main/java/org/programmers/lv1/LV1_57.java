558e67fbe6f46e40ad63eec90ef2ffd95cd703b4
antaeho <an981022@naver.com>
2023-04-20 11:08:39 +0900

    74번 해결

---
+++ src/main/java/org/programmers/lv1/LV1_57.java
@@ -0,0 +1,20 @@
+package org.programmers.lv1;
+
+public class LV1_57 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String t, String p) {
+        int len = p.length();
+        long num = Long.parseLong(p);
+        int result = 0;
+
+        for (int i = 0; i < t.length() - len + 1; i++) {
+            long diff = Long.parseLong(t.substring(i, i + len));
+            if (diff <= num) result++;
+        }
+        return result;
+    }
+}
