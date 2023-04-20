83d71817fa094b12b5d7c06b36e60665031df5c6
antaeho <an981022@naver.com>
2023-04-20 10:10:08 +0900

    68번 해결

---
+++ src/main/java/org/programmers/lv1/LV1_51.java
@@ -0,0 +1,22 @@
+package org.programmers.lv1;
+
+public class LV1_51 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int i = 0; i < number.length; i++) {
+            for(int j = i + 1; j < number.length; j++) {
+                for(int k = j + 1; k < number.length; k++) {
+                    if(number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
