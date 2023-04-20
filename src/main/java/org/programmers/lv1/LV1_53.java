8bc8fadbe420bf663ebb76e33279e4db93567033
antaeho <an981022@naver.com>
2023-04-20 10:32:13 +0900

    70번 해결

---
+++ src/main/java/org/programmers/lv1/LV1_53.java
@@ -0,0 +1,42 @@
+package org.programmers.lv1;
+
+public class LV1_53 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for(int i=0; i<n; i++) {
+            String row = "";
+            String bin1 = Integer.toBinaryString(arr1[i]);
+            String bin2 = Integer.toBinaryString(arr2[i]);
+
+            String[] a1 = addZero(bin1, n);
+            String[] a2 = addZero(bin2, n);
+
+            for(int j=0; j<n; j++) {
+                if("1".equals(a1[j]) || "1".equals(a2[j])) row += "#";
+                else row += " ";
+            }
+
+            answer[i] = row;
+        }
+
+        return answer;
+    }
+
+    public String[] addZero(String bin, int n) {
+        if(bin.length() != n) {
+            String zero = "";
+            for(int i=0; i<n-bin.length(); i++) {
+                zero += "0";
+            }
+            bin = zero + bin;
+        }
+
+        return bin.split("");
+    }
+}
