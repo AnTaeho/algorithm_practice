e8f303d7baa2b70d8cc5598039dd9dbc33b260c1
antaeho <an981022@naver.com>
2023-04-20 11:03:22 +0900

    73번 해결

---
+++ src/main/java/org/programmers/lv1/LV1_56.java
@@ -0,0 +1,20 @@
+package org.programmers.lv1;
+
+import java.util.Arrays;
+
+public class LV1_56 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
