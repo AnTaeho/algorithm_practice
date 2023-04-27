8c30e24fd76939867934ae0e7f04918d9ca3e996
antaeho <an981022@naver.com>
2023-04-27 10:19:44 +0900

    98번 해결

--- src/main/java/org/programmers/lv1/LV1_82.java
+++ src/main/java/org/programmers/lv1/LV1_82.java
@@ -1,5 +1,7 @@
 package org.programmers.lv1;
 
+import java.util.*;
+
 public class LV1_82 {
 
     public static void main(String[] args) {
@@ -7,6 +9,23 @@ public class LV1_82 {
 
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
+        int N = board[0].length;
+        Stack<Integer> stack = new Stack<>();
+        for (int move : moves) {
+            for (int i = 0; i < N; i++) {
+                int num = board[i][move - 1];
+                if (num != 0) {
+                    if (!stack.empty() && stack.get(stack.size()-1) == num) {
+                        stack.pop();
+                        answer += 2;
+                    } else {
+                        stack.push(num);
+                    }
+                    board[i][move-1] = 0;
+                    break;
+                }
+            }
+        }
         return answer;
     }
 
